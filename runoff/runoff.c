55226ec1db374bbaa6f3869cde7e3e891da4d399
shanmengqi2 <69161111+shanmengqi2@users.noreply.github.com>
2025-03-29 21:46:10 +0800

    Update runoff.c

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -181,7 +181,7 @@ int find_min(void)
 {
     // TODO
     int min;
-    for (int i = 0; i < candidate_count - 1; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].eliminated == false)
         {
